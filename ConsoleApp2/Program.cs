ee548fbfcc5aec4176536f540da4d1e80c94717b
yuanjy <yuanjy@yiche.com>
2018-10-19 15:30:26 +0800

    1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Jenkins");
+            Console.WriteLine("Jenkins sss");
             Console.Read();
         }
     }
